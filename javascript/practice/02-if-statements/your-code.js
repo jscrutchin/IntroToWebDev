a6bbc379cb0024820a32ac817c50e7e63819d98a
Jake Scrutchin <scrutchin.jake@gmail.com>
2021-12-01 12:32:45 -0600

    did if statement code along

--- javascript/practice/02-if-statements/your-code.js
+++ javascript/practice/02-if-statements/your-code.js
@@ -7,8 +7,8 @@
 
 // You are given four variables with numeric values.
 
-var kidsWhoWantToSwim = 25;   // number of kids who want to swim
-var kidsWhoWantDoArchery = 5; // number of kids who want to do archery
+var kidsWhoWantToSwim = 12;   // number of kids who want to swim
+var kidsWhoWantDoArchery = 11; // number of kids who want to do archery
 var totalSwimmers = 0;        // number of kids who are actually swimming in the afternoon
 var totalArchers = 0;         // number of kids who are actually doing archery in the afternoon
 
@@ -28,6 +28,14 @@ var totalArchers = 0;         // number of kids who are actually doing archery i
 // ------------------------
 
 // Write your code here:
+if (kidsWhoWantToSwim < 0 || kidsWhoWantDoArchery < 0){
+    console.log('ERROR');
+} else if (kidsWhoWantToSwim % 2 === 0){
+    totalSwimmers = kidsWhoWantToSwim;
+    totalArchers = kidsWhoWantDoArchery;
+} else {
+    totalArchers = kidsWhoWantDoArchery + kidsWhoWantToSwim;
+}
 
 
 
