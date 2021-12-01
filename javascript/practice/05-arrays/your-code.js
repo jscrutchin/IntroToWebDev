6ab9d6a302d6da0b3cc49772c54e2d01b1cd0d50
Jake Scrutchin <scrutchin.jake@gmail.com>
2021-12-01 15:44:23 -0600

    finished code practice arrays, a couple of issues

--- javascript/practice/05-arrays/your-code.js
+++ javascript/practice/05-arrays/your-code.js
@@ -9,20 +9,24 @@
 // Tip: Start with 2 numbers in the array. Once the function works with 2 numbers, add a third, and so on.
 // Write your code here:
 
+function findMax(numbers){
+    return Math.max(...numbers);
+}
+
 
 // Open index.html in your browser, open the JavaScript console, confirm the result.
 
 // Uncomment the code below to verify your function is working.
-// console.log("findMax =====");
-// var numbers = [46, 3, 40, 74, 63];
-// var actual = findMax(numbers);
-// console.log("[46, 3, 40, 74, 63] -> expected: 74, actual: %s", actual);
-// numbers = [3, 3, 3, 3];
-// actual = findMax(numbers);
-// console.log("[3, 3, 3, 3] -> expected: 3, actual: %s", actual);
-// numbers = [-1];
-// actual = findMax(numbers);
-// console.log("[-1] -> expected: -1, actual: %s", actual);
+ console.log("findMax =====");
+ var numbers = [46, 3, 40, 74, 63];
+ var actual = findMax(numbers);
+ console.log("[46, 3, 40, 74, 63] -> expected: 74, actual: %s", actual);
+ numbers = [3, 3, 3, 3];
+ actual = findMax(numbers);
+ console.log("[3, 3, 3, 3] -> expected: 3, actual: %s", actual);
+ numbers = [-1];
+ actual = findMax(numbers);
+ console.log("[-1] -> expected: -1, actual: %s", actual);
 
 
 // 2. COMBINE ARRAYS
@@ -32,16 +36,22 @@
 // The function should NOT alter either parameter.
 // Write your code here:
 
+function combine(arr1, arr2){
+   let arr3 = [...arr1, ...arr2];
+   // console.log(arr3) when i console.log it seemingly works, not sure what the problem is here.
+   return arr3;
+}
+
 
 
 // Uncomment the code below to verify your function is working.
-// console.log("combine =====");
-// var merged = combine(["Oak", "Maple", "Birch"], ["Hydrogen", "Helium"]);
-// console.log("expected: Oak,Maple,Birch,Hydrogen,Helium\nactual: %s", merged);
-// merged = combine([12.5, 3, 855], [true, false, true]);
-// console.log("expected: 12.5,3,855,true,false,true\nactual: %s", merged);
-// merged = combine(["Saturn"], ["Black Lab", "Borzoi", "Pug"]);
-// console.log("expected: Saturn,Black Lab,Borzoi,Pug\nactual: %s", merged);
+ console.log("combine =====");
+ var merged = combine(["Oak", "Maple", "Birch"], ["Hydrogen", "Helium"]);
+ console.log("expected: Oak,Maple,Birch,Hydrogen,Helium\nactual: %s", merged);
+ merged = combine([12.5, 3, 855], [true, false, true]);
+ console.log("expected: 12.5,3,855,true,false,true\nactual: %s", merged);
+ merged = combine(["Saturn"], ["Black Lab", "Borzoi", "Pug"]);
+ console.log("expected: Saturn,Black Lab,Borzoi,Pug\nactual: %s", merged);
 
 
 // 3. CHALLENGE: SUBTRACT AN ARRAY
@@ -53,16 +63,27 @@
 // The function should NOT alter either parameter.
 // Write your code here:
 
+function subtractArray(arr1, arr2){
+    let arr3 = [];
+    for (let i = 0; i < arr1.length; i++){
+        if (arr2.indexOf(arr1[i]) === -1){
+            arr3.push(arr1[i]);
+        }
+    }
+    //console.log(arr3); same problem as the 2nd question
+    return arr3;
+}
+// 
 
 
 // Uncomment the code below to verify your function is working.
-// console.log("subtractArray =====");
-// var result = subtractArray([1, 2, 3, 4, 5], [2, 4, 6]);
-// console.log("expected: 1,3,5\nactual: %s", result);
-// result = subtractArray(["Neptune", "Uranus", "Saturn", "Pluto"], ["Pluto"]);
-// console.log("expected: Neptune,Uranus,Saturn\nactual: %s", result);
-// result = subtractArray([1, 2, 3, 1, 2, 3, 1, 2, 3], [1, 3]);
-// console.log("expected: 2,2,2\nactual: %s", result);
+ console.log("subtractArray =====");
+ var result = subtractArray([1, 2, 3, 4, 5], [2, 4, 6]);
+ console.log("expected: 1,3,5\nactual: %s", result);
+ result = subtractArray(["Neptune", "Uranus", "Saturn", "Pluto"], ["Pluto"]);
+ console.log("expected: Neptune,Uranus,Saturn\nactual: %s", result);
+ result = subtractArray([1, 2, 3, 1, 2, 3, 1, 2, 3], [1, 3]);
+ console.log("expected: 2,2,2\nactual: %s", result);
 
 
 
